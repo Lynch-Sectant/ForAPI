0e6c012c83084a046167d7346ff81c68b0dfb0cc
Lesnichiy26 <75080073+Lesnichiy26@users.noreply.github.com>
2021-02-15 16:09:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ pygame.init()
 size = width, height = 800, 600
 screen = pygame.display.set_mode(size)
 playing = True
+
 while playing:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+        else:
+            pass
