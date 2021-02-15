2ee4ce793b88d1d6f8be262c84a66b39e68b2838
Lesnichiy26 <75080073+Lesnichiy26@users.noreply.github.com>
2021-02-15 16:12:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import pygame
+import requsts
+
+
+def load_image(name, colorkey=None):
+    fullname = (name)
+    # если файл не существует, то выходим
+    image = pygame.image.load(fullname)
+    return image
 
 
 pygame.init()
@@ -10,5 +18,5 @@ while playing:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-        else:
+        if event.type == pygame.MOUSEBUTTONDOWN:
             pass
