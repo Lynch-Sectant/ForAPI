ac306330068c2ec3ce9cfdcc118f2b00f1c03664
Артём  Азука <75079994+ArtyomAzuka@users.noreply.github.com>
2021-02-15 16:22:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,8 @@ pygame.init()
 size = width, height = 1000, 1000
 screen = pygame.display.set_mode(size)
 playing = True
-
+with open("map.png", "wb") as file:
+    file.write(response.content)
 while playing:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
