2c4f4ab6ec449c7d19662cedb0f2ab802bd6376b
Артём  Азука <75079994+ArtyomAzuka@users.noreply.github.com>
2021-02-15 16:12:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import pygame
 import requsts
 
-
+api_server = "http://static-maps.yandex.ru/1.x/"
+params = {
+    "ll": ",".join([0, 0]),
+    "spn": ",".join([90, 90]),
+    "l": "map"}
+response = requests.get(api_server, params=params)
 def load_image(name, colorkey=None):
     fullname = (name)
     # если файл не существует, то выходим
