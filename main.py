bf548ac1ccd98be1451c00b7a469a096e106aa81
Lesnichiy26 <75080073+Lesnichiy26@users.noreply.github.com>
2021-02-15 16:31:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ params = {
     "l": "map"}
 response = requests.get(api_server, params=params)
 pygame.init()
-size = width, height = 1000, 1000
+size = width, height = 1500, 1000
 screen = pygame.display.set_mode(size)
 running = True
 with open("map.png", "wb") as file:
@@ -26,7 +26,7 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         else:
-            fon = pygame.transform.scale(load_image('map.png'), (1000, 1000))
+            fon = pygame.transform.scale(load_image('map.png'), (1500, 1000))
             screen.blit(fon, (0, 0))
-   pygame.display.flip()
+    pygame.display.flip()
 pygame.quit()
