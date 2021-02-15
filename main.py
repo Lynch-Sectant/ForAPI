bc9b7ee923bfab2733d870a47498326bd28b3bf7
Lesnichiy26 <75080073+Lesnichiy26@users.noreply.github.com>
2021-02-15 16:40:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -25,6 +25,11 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+           if event.key == pygame.K_PAGEUP:
+               pass
+           if event.key == pygame.K_PAGEDOWN:
+               pass
         fon = pygame.transform.scale(load_image('map.png'), (1500, 1000))
         screen.blit(fon, (0, 0))
     pygame.display.flip()
