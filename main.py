59bb887eb8dee9a1d2379ebe8b567227844c235b
Артём  Азука <75079994+ArtyomAzuka@users.noreply.github.com>
2021-02-15 16:46:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def load_image(name, colorkey=None):
     image = pygame.image.load(fullname)
     return image
 
+
 scale = 90
 api_server = "http://static-maps.yandex.ru/1.x/"
 params = {
@@ -26,10 +27,20 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
-           if event.key == pygame.K_PAGEUP:
-               pass
-           if event.key == pygame.K_PAGEDOWN:
-               pass
+            if event.key == pygame.K_PAGEUP:
+                if scale != 90:
+                    scale += 10
+            if event.key == pygame.K_PAGEDOWN:
+                if scale != 0:
+                    scale -= 10
+            params["spn"] = ",".join([f'{scale}', f'{scale}'])
+            response = requests.get(api_server, params=params)
+            pygame.init()
+            size = width, height = 1500, 1000
+            screen = pygame.display.set_mode(size)
+            running = True
+            with open("map.png", "wb") as file:
+                file.write(response.content)
         fon = pygame.transform.scale(load_image('map.png'), (1500, 1000))
         screen.blit(fon, (0, 0))
     pygame.display.flip()
